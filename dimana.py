7d1c476633b60b57450bfd62369b5857f8eac442
Nathan Wilcox <nejucomo@gmail.com>
2012-07-30 12:23:37 -0700

    Untested rough draft.

---
+++ dimana.py
@@ -0,0 +1,122 @@
+"""_Dim_ensional _Ana_lysis - Values with associated measurement units.
+
+Example:
+>>> from dimana import Dimensional
+>>> Feet = Dimensional.get_dimension('Feet')
+>>> Lbs = Dimensional.get_dimension('Lbs')
+>>> Sec = Dimensional.get_dimension('Sec')
+>>> Feet(23) * Lbs(13) / (Sec(1) * Sec(1))
+299 [Feet*Lbs/(Sec^2)]
+"""
+
+from decimal import Decimal
+
+
+class Dimensional (object):
+    # Warning: This assumes subclass names are unique across the runtime.
+
+    @staticmethod
+    def get_dimension(name):
+        return Dimensional._get_multi( {name: 1} )
+
+    @staticmethod
+    def _get_multi(dims):
+        key = 'Dimensional_' + '_'.join( '%s_%s' % p for p in sorted(dims.items()))
+    
+        try:
+            return Dimensional._subtype_cache[key]
+        except KeyError:
+            subtype = Dimensional._define_new(key, dims)
+            Dimensional._subtype_cache[key] = subtype
+            return subtype
+        
+    @staticmethod
+    def _define_new(name, dims):
+        rep = Dimensional._stringify_dims(dims)
+
+        clsdict = {
+            '__init__': lambda self: None,
+            '_dims': dims,
+            '__str__': lambda self: rep,
+            }
+
+        return type(name, (Dimensional,), clsdict)
+
+    _subtype_cache = {}
+
+    _dims = {}
+
+    def __init__(self, value):
+        self.value = Decimal(value)
+
+    @property
+    def dimstr(self):
+        poses = []
+        negs = []
+        for (unit, power) in self._dims.iteritems():
+            assert power != 0, `self._dims`
+            abspower = abs(power)
+
+            s = unit
+            if abspower != 1:
+                s += '^%d' % abspower
+
+            if power > 0:
+                poses.append(s)
+            else:
+                negs.append(s)
+
+        posstr = '*'.join(poses)
+        negstr = '*'.join(negs)
+        if negs:
+            if poses:
+                if len(poses) > 1:
+                    posstr = '(%s)' % (posstr,)
+            else:
+                posstr = '1'
+            if len(negs) > 1:
+                negstr = '(%s)' % (negstr,)
+
+            result = '%s / %s' % (posstr, negstr)
+        else:
+            result = posstr
+        return '[%s]' % (result,)
+
+    @property
+    def inverse(self):
+        return type(self)._get_multi(dict( (k, -v) for (k, v) in self._dims.iteritems() ))
+
+    def __cmp__(self, other):
+        typecheck(other, type(self))
+        return cmp(self.value, other.value)
+
+    def __add__(self, other):
+        typecheck(other, type(self))
+        return type(self)(self.value + other.value)
+
+    def __sub__(self, other):
+        return self + ( - other )
+
+    def __neg__(self):
+        return type(self)( - self.value )
+
+    def __mul__(self, other):
+        typecheck(other, Dimensional)
+
+        dims={}
+        for key in set(self._dims.keys() + other._dims.keys()):
+            sumpower = self._dims.get(key, 0) + other._dims.get(key, 0)
+            if sumpower != 0:
+                dims[key] = sumpower
+
+        cls = Dimensional._get_multi(dims)
+        return cls(self.value * other.value)
+
+    def __div__(self, other):
+        return self * other.inverse
+
+
+def typecheck(i, t):
+    if not isinstance(i, t):
+        raise TypeError('Type %r does not include instance %r' % (t, i))
+
