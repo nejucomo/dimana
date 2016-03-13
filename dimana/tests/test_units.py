e8cf0e3411065a6a646ed68ccee31893b8255daf
Nathan Wilcox <nejucomo@gmail.com>
2016-03-13 13:01:20 -0700

    Add test spec and impl for arithmetic operator overloading.

--- dimana/tests/test_units.py
+++ dimana/tests/test_units.py
@@ -4,8 +4,49 @@ import unittest
 from dimana.units import Units
 
 
-class UnitsTests (unittest.TestCase):
+class UnitsValueTypeTests (unittest.TestCase):
     def test_value_type_construction(self):
         u1 = Units({})
         u2 = Units({})
         self.assertIs(u1, u2)
+
+        u1 = Units({'meter': 1})
+        u2 = Units({'meter': 1})
+        self.assertIs(u1, u2)
+
+        u1 = Units({'meter': 1, 'sec': -2})
+        u2 = Units({'meter': 1, 'sec': -2})
+        self.assertIs(u1, u2)
+
+
+class UnitsArithmeticOperationsTests (unittest.TestCase):
+    def setUp(self):
+        self.m = Units({'meter': 1})
+        self.s = Units({'sec': 1})
+
+    def test__mul__(self):
+        self.assertIs(
+            Units({'meter': 1, 'sec': 1}),
+            self.m * self.s,
+        )
+
+    def test__mul__TypeError(self):
+        self.assertRaises(TypeError, self.m.__mul__, 42)
+
+    def test__div__(self):
+        self.assertIs(
+            Units({'meter': 1, 'sec': -1}),
+            self.m / self.s,
+        )
+
+    def test__div__TypeError(self):
+        self.assertRaises(TypeError, self.m.__div__, 42)
+
+    def test__pow__(self):
+        self.assertIs(
+            Units({'meter': 3}),
+            self.m ** 3,
+        )
+
+    def test__truediv__is__div__(self):
+        self.assertEqual(Units.__truediv__, Units.__div__)
--- dimana/units.py
+++ dimana/units.py
@@ -13,5 +13,30 @@ class Units (object):
             self._dimpowers = dimpowers
         # Else: this instance has already been initialized.
 
+    def __mul__(self, other):
+        if isinstance(other, Units):
+            newdp = {}
+            tmpdp = dict(self._dimpowers)
+            for uname, power in other._dimpowers.iteritems():
+                newdp[uname] = power + tmpdp.pop(uname, 0)
+            newdp.update(tmpdp)
+            return Units(newdp)
+        else:
+            raise TypeError(
+                'Units multiplied against non-Units {!r}'
+                .format(type(other))
+            )
+
+    def __div__(self, other):
+        return self * (other ** (-1))
+
+    __truediv__ = __div__
+
+    def __pow__(self, p):
+        newdp = {}
+        for uname, power in self._dimpowers.iteritems():
+            newdp[uname] = power * p
+        return Units(newdp)
+
     # Private:
     _instances = {}
